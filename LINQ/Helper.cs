3418cce73be3799000375ecfdc431efe191772bd
Kristina Kulich <kristina.kulitsch@gmail.com>
2023-09-16 13:56:34 +0200

    adding LINQ Homework

---
+++ LINQ/Helper.cs
@@ -0,0 +1,11 @@
+﻿namespace LINQ
+{
+    public static class Helper
+    {
+        private static int iseven;
+        public static bool CheckIfIsDevidedWithoutRamainder(int number, int devider)
+        {
+            return number % devider == 0;
+        }
+    }
+}
---
+++ LINQ/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ
+{
+        public class Student
+        {
+                public int StudentID { get; set; }
+                public string StudentName { get; set; }
+                public int Grade { get; set; }
+                public int StandardID { get; set; }
+        }
+}
